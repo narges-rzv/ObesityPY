c1fff520a1a9f50da42b7bc672a5cb114fe66e37
narges razavian <narges.razavian@nyumc.org>
2017-07-12 19:02:08 -0400

    added mlp

--- src/train_temporal.py
+++ src/train_temporal.py
@@ -7,8 +7,6 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as functionalnn
 import torch.optim as optim
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import time
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -22,10 +20,12 @@ def preprocess(x):
         x[:,ix,:][((x[:,ix,:] - m ) > 3*s) ] = 0
         print(ix, m, s)
 
-def load_data(TimeIn=[0,18*12], Timeout=[0, 18*12], outcomeIx=0):
+def load_data(TimeIn=[1*12,3*12+1], Timeout=[5*12-3, 5*12+3], outcomeIx=0):
     #(data, data_percentile, datakeys, datagenders)
-    (d, dp, dk, dg, dethn, drace) = pickle.load(open('timeseries_data20170620-174720.pkl','rb'))
-    vitals = ['BMI', 'HC', 'Ht', 'Wt']
+    TimeIn = [int(i) for i in TimeIn]
+    Timeout = [int(i) for i in Timeout]
+    (d, dp, dk, dg, dethn, drace) = pickle.load(open('timeseries_data20170711-154021.pkl','rb'))
+    vitals = ['BMI', 'HC', 'Ht Percentile', 'Wt Percentile'] #['BMI', 'HC', 'Ht', 'Wt']
     #dimension of data is N x |vitals| x |time=18*12mnths|
 
     gender_streched = np.repeat(np.array(dg).reshape(len(dg),1,1), d.shape[2], axis=2)
@@ -36,8 +36,8 @@ def load_data(TimeIn=[0,18*12], Timeout=[0, 18*12], outcomeIx=0):
     d = np.concatenate([d, gender_streched, ethn_streched, race_streched], axis=1)
     preprocess(d)
 
-    print ('total num of ppl with any of the vitals measured at age 0-24months:', ((d[:,0:len(vitals),TimeIn[0]:TimeIn[1]].sum(axis=2)>0).sum(axis=1)>0).sum())
-    print ('total num of ppl with BMI measured at age 4-6:', ((d[:, outcomeIx, Timeout[0]:Timeout[1]].sum(axis=1)>0)).sum() )
+    print ('total num of ppl with any of the vitals measured at age '+ str(TimeIn[0]) + ' to '+str(TimeIn[1])+'(mnth):', ((d[:,0:len(vitals),TimeIn[0]:TimeIn[1]].sum(axis=2)>0).sum(axis=1)>0).sum())
+    print ('total num of ppl with BMI measured at age '+ str(Timeout[0]) + ' to '+str(Timeout[1])+'(mnth):', ((d[:, outcomeIx, Timeout[0]:Timeout[1]].sum(axis=1)>0)).sum() )
     print ('total num of ppl with both of above consitions:', (((d[:, outcomeIx, Timeout[0]:Timeout[1]].sum(axis=1)>0)) & ((d[:,0:len(vitals), TimeIn[0]:TimeIn[1]].sum(axis=2)>0).sum(axis=1)>0)).sum() ) 
 
     ix_selected_cohort = (((d[:, outcomeIx, Timeout[0]:Timeout[1]].sum(axis=1)>0)) & ((d[:, 0:len(vitals), TimeIn[0]:TimeIn[1]].sum(axis=2)>0).sum(axis=1)>0))
@@ -87,9 +87,14 @@ def impute(batchInputNumpy):
             batchInputNumpy[:,dataix, dim] = gp.predict(xpred).ravel() + y.mean()
             # return(X,y,gp.predict(xpred).ravel() + y.mean() )
  
+def plot_progress(trainloss, trainepoch, validloss, validepoch):
+    plt.plot(trainepoch, trainloss)
+    plt.plot(validepoch, validloss)
+    plt.draw()
+    plt.pause(1)
 
 class LSTMPredictor_singleclassifier(nn.Module):
-    def __init__(self, hidden_dim, input_dim, num_layers, dout, bfirst, tagset_size, minibatch_size, time_dim, bidirectional):
+    def __init__(self, hidden_dim, input_dim, num_layers, dout, bfirst, target_size, minibatch_size, time_dim, bidirectional):
         super(LSTMPredictor_singleclassifier, self).__init__()
         self.hidden_dim = hidden_dim
         self.minibatch_size = minibatch_size
@@ -99,22 +104,23 @@ class LSTMPredictor_singleclassifier(nn.Module):
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, dropout=dout, batch_first=bfirst, bidirectional=bidirectional)
 
         # The linear layer that maps from hidden state space to tag space
-        self.hidden2output = nn.Linear(hidden_dim * time_dim * self.directions, tagset_size)
+        self.hidden2output = nn.Linear(hidden_dim * self.directions * self.time_dim , target_size)
         self.hidden = self.init_hidden(minibatch_size)
 
     def init_hidden(self,minibatch_size): # The axes semantics are (num_layers, minibatch_size, hidden_dim)
-        return (autograd.Variable(torch.zeros(self.minibatch_size, self.num_layers * self.directions,  self.hidden_dim)),
-                autograd.Variable(torch.zeros(self.minibatch_size, self.num_layers * self.directions,  self.hidden_dim)))
+        return (autograd.Variable(torch.zeros( self.num_layers * self.directions, self.minibatch_size, self.hidden_dim)),
+                autograd.Variable(torch.zeros( self.num_layers * self.directions, self.minibatch_size, self.hidden_dim)))
     
     def forward(self, input):        
         lstm_out, self.hidden = self.lstm(input)
         # lstm_out_trans = (torch.transpose(lstm_out.contiguous(), 0, 1))
-        net_out1 = self.hidden2output(lstm_out.contiguous().view(self.minibatch_size, self.hidden_dim*self.time_dim*self.directions))
+        # print(lstm_out.size())
+        net_out1 = self.hidden2output(lstm_out[:,:,:].contiguous().view(self.minibatch_size, self.hidden_dim * self.time_dim * self.directions))
         # net_softmaxout = functionalnn.log_softmax(net_out)
         return net_out1
 
 class LSTMPredictor_multiclassifier(nn.Module):
-    def __init__(self, hidden_dim, input_dim, num_layers, dout, bfirst, tagset_size, minibatch_size, time_dim, bidirectional):
+    def __init__(self, hidden_dim, input_dim, num_layers, dout, bfirst, target_size, minibatch_size, time_dim, bidirectional):
         super(LSTMPredictor_multiclassifier, self).__init__()
         self.hidden_dim = hidden_dim
         self.minibatch_size = minibatch_size
@@ -122,43 +128,82 @@ class LSTMPredictor_multiclassifier(nn.Module):
         self.num_layers = num_layers
         self.directions = 2 if bidirectional else 1
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, dropout=dout, batch_first=bfirst, bidirectional=bidirectional)
-        self.lstm = nn.RNN(input_dim, hidden_dim, num_layers, dropout=dout, batch_first=bfirst, bidirectional=bidirectional)
 
-    def init_hidden(self,minibatch_size): # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+    def init_hidden(self, minibatch_size): # The axes semantics are (num_layers, minibatch_size, hidden_dim)
         return (autograd.Variable(torch.zeros(self.minibatch_size, self.num_layers * self.directions,  self.hidden_dim)),
                 autograd.Variable(torch.zeros(self.minibatch_size, self.num_layers * self.directions,  self.hidden_dim)))
     
     def forward(self, input):        
         lstm_out, self.hidden = self.lstm(input)
-        return lstm_out
+        return lstm_out[-1]
 
-def build_train_lstm(dIn, dOut, dInValid, dOutValid, dInTest,dOutTest, model_file='obesityat520170620-105723.pyth_lstm', 
-    hidden_dim=18, dropout=False, batch_size=8, num_layers=1, gap=0, bidirectional=False):
+class MLPPredictor(nn.Module):
+    def __init__(self, hidden_dim, input_dim, num_layers, dout, target_size, time_dim, minibatch_size):
+        super(MLPPredictor, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.time_dim = time_dim
+        self.num_layers = num_layers
+        self.minibatch_size = minibatch_size
+        self.mlp = []
+        current_size =  time_dim * input_dim
+        for i in range(0, self.num_layers - 1):
+            self.mlp.append(nn.Linear(current_size, hidden_dim))
+            self.mlp.append(nn.ReLU())
+            current_size = hidden_dim
+        self.mlp.append(nn.Linear(current_size, target_size))
+        print(self.mlp)
+        self.parameters = []
+        for m in self.mlp:
+            self.parameters.append(m.parameters())
+            print(self.parameters)
+        self.parameters = nn.ParameterList(self.parameters)
+
+    def forward(self, input):
+        print(out.size())
+        out = self.mlp[0](input.view(self.minibatch_size, self.input_dim*self.time_dim))
+        for m in self.mlp[1:]:
+            print(out.size())
+            out = m(out)
+        return out
+
+
+def build_train_dnn(dIn, dOut, dInValid, dOutValid, dInTest,dOutTest, model_file='xxx.pyth_pkl', 
+    hidden_dim=100, dropout=0.5, batch_size=16, num_layers=3, gap=0, bidirectional=False, batch_first=False, model_type='mlp'):
     # set ramdom seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
     random.seed(0)
+    avgTrainLoss, avgValidLoss, trainepoch, validepoch = [], [], [], []
+    plt.ion()
+    plt.show()
     # load data and make training set
     print('input data:', dIn.shape, dIn.__class__)
-    dInputTransposed = dIn.transpose((0,2,1)).copy()
-    dOutputTransposed = dOut.transpose().max(axis=0)    
-    print('output data:', dOut.shape, dOut.__class__)
-    dInValidtrans, dOutValidtrans = dInValid.transpose((0,2,1)).copy(), dOutValid.transpose().max(axis=0)
-    dInTesttrans, dOutTesttrans = dInTest.transpose((0,2,1)).copy(), dOutTest.transpose().max(axis=0)
+    if model_type in ['lstm1', 'lstm2']:
+        dInputTransposed = dIn.transpose((2,0,1)).copy()
+        dOutputTransposed = dOut.transpose().max(axis=0)    
+        print('output data:', dOut.shape, dOut.__class__)
+        dInValidtrans, dOutValidtrans = dInValid.transpose((2,0,1)).copy(), dOutValid.transpose().max(axis=0)
+        dInTesttrans, dOutTesttrans = dInTest.transpose((2,0,1)).copy(), dOutTest.transpose().max(axis=0)
+    elif model_type in ['mlp']:
+        dInputTransposed = dIn.copy()
+        dOutputTransposed = dOut.transpose().max(axis=0)    
+        print('output data:', dOut.shape, dOut.__class__)
+        dInValidtrans, dOutValidtrans = dInValid.copy(), dOutValid.transpose().max(axis=0)
+        dInTesttrans, dOutTesttrans = dInTest.copy(), dOutTest.transpose().max(axis=0)
     seq_size = dIn.shape[2] - gap
     input_dim = dIn.shape[1]
     target_dim = 1
     totalbatches = int(dIn.shape[0]/batch_size)
 
-    model1 = LSTMPredictor_singleclassifier(hidden_dim, input_dim, num_layers, dropout, True, target_dim, batch_size, seq_size, bidirectional)
-    
-    model2 = LSTMPredictor_multiclassifier(hidden_dim, input_dim, num_layers, dropout, True, target_dim, batch_size, seq_size, bidirectional)
-    
-    model = model1
+    if model_type == 'lstm1':
+        model = LSTMPredictor_singleclassifier(hidden_dim, input_dim, num_layers, dropout, batch_first, target_dim, batch_size, seq_size, bidirectional)
+    elif model_type == 'lstm2':
+        model = LSTMPredictor_multiclassifier(hidden_dim, input_dim, num_layers, dropout, batch_first, target_dim, batch_size, seq_size, bidirectional)
+    elif model_type == 'mlp':
+        model = MLPPredictor(hidden_dim, input_dim, num_layers, dropout, target_dim, seq_size, batch_size)
 
     loss_function = nn.MSELoss() #if classification nn.NLLLoss()
-    
-    optimizer = optim.RMSprop(model.parameters(), lr=0.05)
+    optimizer = optim.Adam(model.parameters(), lr=0.01) #optim.Adam is faster
     
     try:
         model = torch.load(model_file)
@@ -172,22 +217,29 @@ def build_train_lstm(dIn, dOut, dInValid, dOutValid, dInTest,dOutTest, model_fil
             break
         total_loss = 0
         total_cnt = 0
-        ix_shuffle = range(0,dInputTransposed.shape[0])
+        ix_shuffle = range(0,dIn.shape[0])
         random.shuffle(list(ix_shuffle))
-        dInputTransposed_shuffled = dInputTransposed[ix_shuffle,:,:]
+        dInputTransposed_shuffled = dInputTransposed[:,ix_shuffle,:]
         dOutputTransposed_shuffled = dOutputTransposed[ix_shuffle]
         for batchIx in range(0, totalbatches):
-            batchInputNumpy = dInputTransposed_shuffled[(batchIx*batch_size):(batchIx*batch_size) + batch_size, 0:seq_size-gap, 0:input_dim]
+            
+            if model_type in ['lstm1', 'lstm2']:
+                batchInputNumpy = dInputTransposed_shuffled[0:seq_size-gap, (batchIx*batch_size):(batchIx*batch_size) + batch_size, 0:input_dim]
+            elif model_type in ['mlp']:
+                batchInputNumpy = dInputTransposed_shuffled[(batchIx*batch_size):(batchIx*batch_size) + batch_size, 0:input_dim, 0:seq_size-gap,]
+            
             batchInput = torch.from_numpy(batchInputNumpy).float()
             batchTarget1 = torch.from_numpy(dOutputTransposed_shuffled[(batchIx*batch_size):(batchIx*batch_size) + batch_size]).float()
-            batchTarget2 = torch.from_numpy(dInputTransposed_shuffled[(batchIx*batch_size):(batchIx*batch_size) + batch_size, gap:seq_size, 0:input_dim]).float()
-
-            batchTarget = batchTarget1
+            batchTarget2 = torch.from_numpy(dInputTransposed_shuffled[gap:seq_size, (batchIx*batch_size):(batchIx*batch_size) + batch_size, 0:input_dim]).float()
+            if model_type in ['lstm1', 'mlp']:
+                batchTarget = batchTarget1
+            elif model_type in ['lstm2']:
+                batchTarget = batchTarget2
 
             model.zero_grad()
-            model.hidden = model.init_hidden(batch_size)
+            if model_type in ['lstm1', 'mlp']:
+                model.hidden = model.init_hidden(batch_size)
             predictions = model(Variable(batchInput))
-            # print('lstm input:', batchInput.size(), 'lstm output:',predictions.size(), 'target:',batchTarget.size())
             loss = loss_function(predictions, Variable(batchTarget))
             total_loss += loss.data.numpy()[0]
             total_cnt += 1
@@ -195,28 +247,40 @@ def build_train_lstm(dIn, dOut, dInValid, dOutValid, dInTest,dOutTest, model_fil
             optimizer.step()
 
         print('average Train mse loss at epoch:', epoch, ' is:',total_loss/total_cnt)
+        avgTrainLoss.append(total_loss/total_cnt)
+        trainepoch.append(epoch)
+        plot_progress(avgTrainLoss, trainepoch, avgValidLoss, validepoch)
+
         if (epoch % 10) == 0 :
             valid_loss = 0
             total_cnt_valid = 0
             for ixvalidBatch in range(0,int(len(dOutValid)/batch_size)):
-                validBatchIn = torch.from_numpy(dInValidtrans[(ixvalidBatch*batch_size):(ixvalidBatch*batch_size) + batch_size, 0:seq_size-gap, :]).float()
+                if model_type in ['lstm1', 'lstm2']:
+                    validBatchIn = torch.from_numpy(dInValidtrans[0:seq_size-gap, (ixvalidBatch*batch_size):(ixvalidBatch*batch_size) + batch_size, :]).float()
+                elif model_type in ['mlp']:
+                    validBatchIn = torch.from_numpy(dInValidtrans[(ixvalidBatch*batch_size):(ixvalidBatch*batch_size) + batch_size, :, 0:seq_size-gap]).float()
                 validbatchOut1 = torch.from_numpy(dOutValidtrans[(ixvalidBatch*batch_size):(ixvalidBatch*batch_size) + batch_size]).float()
-                validbatchOut2 = torch.from_numpy(dInValidtrans[(ixvalidBatch*batch_size):(ixvalidBatch*batch_size) + batch_size, gap:seq_size, :]).float()
-                validbatchOut = validbatchOut1
+                validbatchOut2 = torch.from_numpy(dInValidtrans[gap:seq_size, (ixvalidBatch*batch_size):(ixvalidBatch*batch_size) + batch_size, :]).float()
+                if model_type in ['lstm1', 'mlp']:
+                    validbatchOut = validbatchOut1
+                elif model_type in ['lstm2']:
+                    validbatchOut = validbatchOut2
 
                 validPred = model(Variable(validBatchIn))
                 loss = loss_function(validPred, Variable(validbatchOut))
                 valid_loss += loss.data.numpy()[0]
                 total_cnt_valid += 1
                 timestr = time.strftime("%Y%m%d-%H%M%S")
-                torch.save(model, 'obesityat5'+timestr+'.pyth_lstm')
+                # torch.save(model, 'obesityat5'+timestr+'.pyth_lstm')
             print('   average Valid mse loss at epoch:', epoch, ' is:',valid_loss/total_cnt_valid)
+            avgValidLoss.append(valid_loss/total_cnt_valid)
+            validepoch.append(epoch)
 
     test_pred_all = np.zeros((dOutTesttrans.shape[0]),dtype=float)
     test_loss = 0
     total_cnt_test = 0
     for ixtestBatch in range(0,int(len(dOutTest)/batch_size)):
-        testBatchIn = torch.from_numpy(dInTesttrans[(ixtestBatch*batch_size):(ixtestBatch*batch_size) + batch_size, 0:seq_size, :]).float()
+        testBatchIn = torch.from_numpy(dInTesttrans[0:seq_size, (ixtestBatch*batch_size):(ixtestBatch*batch_size) + batch_size, :]).float()
         testBatchOut = torch.from_numpy(dOutTesttrans[(ixtestBatch*batch_size):(ixtestBatch*batch_size) + batch_size]).float()
         testPred = model(Variable(testBatchIn))
         test_pred_all[(ixtestBatch*batch_size):(ixtestBatch*batch_size) + batch_size] = testPred.data.numpy().ravel().copy()
